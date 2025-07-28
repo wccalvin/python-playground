4282f45faa37cca8de756f983ea8c606f17944c4
wccalvin <clayton@wccalvin.com>
2025-07-28 05:27:44 -0400

    example of __str__

--- oop/operator_overloading.py
+++ oop/operator_overloading.py
@@ -7,9 +7,14 @@ class Point:
     x = self.x + point.x
     y = self.y + point.y
     return Point(x, y)
+  
+  def __str__(self):
+    return f"Point({self.x}, {self.y})"
 
 p1 = Point(3, 4)
 p2 = Point(4, 3)
+print(p1)
+print(p2)
 
 p3 = p1 + p2
-print(f"p3 is ({p3.x}, {p3.y})")
+print(f"p3 is {p3}")
